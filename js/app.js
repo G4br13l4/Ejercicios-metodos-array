3bbfae4746bec39995276c948b30a6aaf9a68693
biokah <maria.anaisr@gmail.com>
2018-02-18 13:15:04 -0600

    Completando ejercicio 2

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
 
 // Ejercicio 1
-var capitalize = function(str) {
-  var splited = (str.split(''))
-  var capitals = []
+function capitalize(str) {
+  var splited = (str.split(''));
+  var capitals = [];
   splited.forEach(function(element){
-    capitals.push(element.toUpperCase())
-  })
-  capitals = capitals.join('')
-  console.log(capitals)
+    capitals.push(element.toUpperCase());
+  });
+  capitals = capitals.join('');
+  console.log(capitals);
 }
 
-var outputCapitalize = capitalize("whoop")
-console.log(outputCapitalize); //----> "WHOOP";
+function capitalizeMap(str) {
+  str = str.split('');
+  str = str.map(function(x) {
+    return x.toUpperCase();
+  }).join('');
+  console.log(str);
+}
 
-
+capitalize('whoop');
+capitalizeMap('whoop');
 
 // Ejercicio 2
-var swapCase = function(str) {
+function swapCase(str) {
   
 }
 
@@ -26,9 +32,15 @@ var swapCase = function(str) {
 
 
 // Ejercicio 3
-var shiftLetters = function(str) {
-  
+function shiftLetters(str) {
+  str = str.split('');
+  str = str.map(function(x) {
+    return String.fromCharCode(x.charCodeAt() + 1);
+  }).join('')
+  console.log(str);
 }
+
+shiftLetters('hello')
 
 //var outputShiftLetters = shiftLetters('hello');
 //console.log(outputShiftLetters); // ---> 'ifmmp'
@@ -44,22 +56,22 @@ var evenNumbers = function(array) {
 };
 
 var outputEvenNumbers = evenNumbers(numberArray);
-console.log(outputEvenNumbers); // ---> [2, 4, 6, 8, 10]
+console.log(outputEvenNumbers); // ---> [2, 4, 6, 8, 10];
 
 
 // Ejercicio 5
 var oddNumbers = function(array) {
   return array.filter(function(number){
     return number % 2 != 0;
-  })
+  });
 };
 
 var outputOddNumbers = oddNumbers(numberArray);
-console.log(outputOddNumbers); // ---> [1, 3, 5, 7, 9]
+console.log(outputOddNumbers); // ---> [1, 3, 5, 7, 9];
 
 
 
-// Ejercicio 5
+// Ejercicio 6
 var reducer = function() {
   
 
@@ -83,32 +95,42 @@ var javascripts = [];
 function jsFilter(){
   persons.forEach(function(element){
     if(element['tags'] == 'javascript'){
-      javascripts.push(element['tags'])
+      javascripts.push(element['tags']);
     }
   })
-  console.log(javascripts)
+  console.log(javascripts);
 
 }
-jsFilter()
-
+jsFilter();
 
 
 // Ejercicio 8
 var paintPersons = function() {
-  var container = document.getElementById('container')
-  persons.forEach(function(element){
-    var id = document.createElement('p')
-    id.innerText = element['id']
-    container.appendChild(id)
-    var name = document.createElement('p')
-    name.innerText = element['name']
-    container.appendChild(name)
-    var tags = document.createElement('p')
-    tags.innerText = element['tags']
-    container.appendChild(tags)
+  var container = document.getElementById('container');
+  persons.forEach(function(element) {
+    personId(element);
+    personName(element);
+    personTag(element);
   })
 }
 
+function personId(element) {
+  var id = document.createElement('p');
+  id.innerText = element['id'];
+  container.appendChild(id);
+}
+
+function personName(element) {
+  var name = document.createElement('p');
+  name.innerText = element['name'];
+  container.appendChild(name);
+}
+
+function personTag(element) {
+  var tags = document.createElement('p');
+  tags.innerText = element['tags'];
+  container.appendChild(tags);
+}
 var outputPaintPerson = paintPersons(persons);
   
 
@@ -141,33 +163,45 @@ function getAge(){
   var counter = 0;
   data.forEach(function(element){
     if(element['type'] === 'dog'){
-      var dogAge = element['age'] * 7
-      counter = counter + dogAge
+      var dogAge = element['age'] * 7;
+      counter = counter + dogAge;
     }
   })
-console.log(counter)
+console.log(counter);
 }
-getAge()
+getAge();
 
 // Ejercicio 10
  var paintPets = function(array) {
   var container = document.getElementById('container')
   data.forEach(function(element) {
-    var name = document.createElement('p')
-    name.innerText = element['name']
-    container.appendChild(name)
-    var age = document.createElement('p')
-    age.innerText = element['age']
-    container.appendChild(age)
-    var type = document.createElement('p')
-    type.innerText = element['type']
-    container.appendChild(type)
+    petName(element);
+    petAge(element);
+    petType(element);
 });
  };
+
+function petName(element) {
+  var name = document.createElement('p');
+  name.innerText = element['name'];
+  container.appendChild(name);
+}
+
+function petAge(element) { 
+  var age = document.createElement('p');
+  age.innerText = element['age'];
+  container.appendChild(age);
+}
+
+function petType(element) {
+  var type = document.createElement('p');
+  type.innerText = element['type'];
+  container.appendChild(type);
+}
 
 var outputPaintPets = paintPets(data);
 
 // Ejercicio 11
 var fruits = ['Banana', 'Orange', 'Apple', 'Kiwi'];
-
-var sortedFruits = fruits.sort()
+var sortedFruits = fruits.sort();
+console.log(sortedFruits);
